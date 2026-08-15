82f8cba4e706e4deae308853e0e5283e8bb74cfd
uchihadani <wenderlypa@gmail.com>
2026-08-15 00:32:33 -0300

    Base de la pagina web

---
+++ comidas/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def inicio(request):
+    return HttpResponse("Bienvenido a la página de recetas de Ana")
